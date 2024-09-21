239c92f2612bdd4a43f5de5d353192f00ed03ffd
ootidea <ootidea@gmail.com>
2024-09-21 10:10:54 +0900

    chore: Cloudflare pages build test

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,11 @@
 import { defineConfig } from '@solidjs/start/config'
 
-export default defineConfig({})
+export default defineConfig({
+  server: {
+    // from https://ryanjc.com/blog/solidstart-cloudflare-pages/
+    preset: 'cloudflare-pages',
+    rollupConfig: {
+      external: ['__STATIC_CONTENT_MANIFEST', 'node:async_hooks'],
+    },
+  },
+})
