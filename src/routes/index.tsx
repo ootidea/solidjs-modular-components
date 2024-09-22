f41b48ca25978543d9ae41d48e4f000c49091e30
ootidea <ootidea@gmail.com>
2024-09-23 07:00:32 +0900

    fix: redirect url

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,5 +2,5 @@ import { Navigate } from '@solidjs/router'
 
 export default function RootPage() {
   // TODO: Temporary redirect in place. Implement proper root page and remove this redirect.
-  return <Navigate href="/components/Button" />
+  return <Navigate href="/Button" />
 }
