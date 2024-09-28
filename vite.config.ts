3b4ff7b80c36e175780cc8e8e7cb9b4201181da3
ootidea <ootidea@gmail.com>
2024-09-29 07:01:57 +0900

    chore: setup build as npm package

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
+import * as path from 'node:path'
+import copy from 'rollup-plugin-copy'
 import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
+  plugins: [
+    solidPlugin(),
+    copy({
+      targets: [
+        {
+          src: 'src/library/**/*.css',
+          dest: 'dist',
+          // Keep the directory structure
+          rename: (name, extension, fullPath) =>
+            path.join(
+              path.relative(path.join(__dirname, 'src/library'), path.dirname(fullPath)),
+              `${name}.${extension}`,
+            ),
+        },
+      ],
+      hook: 'writeBundle',
+    }),
+  ],
   build: {
     lib: {
       entry: 'src/library/index.ts',
@@ -8,11 +29,16 @@ export default defineConfig({
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['solid-js'],
+      external: ['solid-js', 'solid-js/web', /\.css$/],
       output: {
         preserveModules: true,
         preserveModulesRoot: 'src/library',
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '~': '/src',
     },
   },
 })
