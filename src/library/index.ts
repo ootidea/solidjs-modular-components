b985b2d3fd2a78419d7a1925bba6a5d33a212f36
ootidea <ootidea@gmail.com>
2024-10-12 16:37:36 +0900

    fix: prepair npm publish

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,4 +1,7 @@
 export { Button } from './Button/Button'
-export { Modal } from './Modal/Modal'
+export { IconButton } from './IconButton/IconButton'
+export { TextInput } from './TextInput/TextInput'
 export { NumberInput } from './NumberInput/NumberInput'
+export { Modal } from './Modal/Modal'
 export { Icon } from './Icon/Icon'
+export { Spinner } from './Spinner/Spinner'
